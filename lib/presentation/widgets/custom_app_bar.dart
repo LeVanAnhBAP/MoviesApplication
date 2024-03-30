d4b741ae1c426eca78992752ace66e206e06a651
anhlv@bap <anhxtanh1411998@gmail.com>
2024-03-30 11:15:46 +0700

    common: custom appbar

---
+++ lib/presentation/widgets/custom_app_bar.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:movies_app/assets.gen.dart';
+import 'package:movies_app/presentation/widgets/search_text_field.dart';
+
+class IconOptions {
+  final double size;
+  final BorderRadiusGeometry borderRadius;
+  final Color? color;
+
+  const IconOptions({
+    this.size = 24,
+    this.color,
+    this.borderRadius = const BorderRadius.all(Radius.circular(24)),
+  });
+}
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  final String? appBarTitle;
+  final TextStyle? textStyleTitle;
+  final double? titleSpace;
+  final TextAlign titleAlign;
+  final EdgeInsetsGeometry? titleTextMargin;
+  final EdgeInsetsGeometry? searchMargin;
+  final double? widthIconBack;
+  final String? placeholderSearch;
+  final double appBarHeight;
+  final double searchHeight;
+  final Function()? onSettingsPress;
+  final Function()? onBackPress;
+  final Function(String)? onSearchValue;
+
+  const CustomAppBar({
+    Key? key,
+    this.appBarTitle,
+    this.textStyleTitle,
+    this.searchHeight = 40,
+    this.onBackPress,
+    this.titleTextMargin = const EdgeInsets.symmetric(horizontal: 8),
+    this.searchMargin = const EdgeInsets.symmetric(horizontal: 8),
+    this.onSettingsPress,
+    this.titleAlign = TextAlign.start,
+    this.onSearchValue,
+    this.titleSpace = 0,
+    this.widthIconBack = 40,
+    this.placeholderSearch,
+    this.appBarHeight = kToolbarHeight,
+  }) : super(key: key);
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => Size.fromHeight(appBarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  bool _isSearching = false;
+
+  void _toggleSearch() {
+    setState(() {
+      _isSearching = !_isSearching;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: _buildLeading(),
+      titleSpacing: widget.titleSpace,
+      leadingWidth: widget.widthIconBack,
+      title: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 1000),
+        child: !_isSearching ? _buildTitle() : _buildSearch(),
+      ),
+      actions: _buildActions(),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Container(
+      margin: widget.titleTextMargin,
+      width: double.infinity,
+      child: Text(
+        widget.appBarTitle ?? '',
+        textAlign: widget.titleAlign,
+        style: widget.textStyleTitle,
+      ),
+    );
+  }
+
+  Widget _buildSearch() {
+    return Container(
+      width: double.infinity,
+      height: widget.searchHeight,
+      alignment: Alignment.center,
+      padding: widget.searchMargin,
+      child: SearchField(
+        onSearchValue: (value) {
+          if (widget.onSearchValue != null) {
+            widget.onSearchValue!;
+          }
+        },
+        placeholder: widget.placeholderSearch,
+      ),
+    );
+  }
+
+  List<Widget>? _buildActions() {
+    return [
+      if (widget.onSearchValue != null)
+        _buildItemAction(
+          padding: const EdgeInsets.only(right: 16),
+          child: SvgPicture.asset(
+            _isSearching
+                ? Assets.icons.svg.icCloseSeach.path
+                : Assets.icons.svg.icSearch.path,
+            fit: BoxFit.contain,
+          ),
+          onTap: _toggleSearch,
+        ),
+      if (!_isSearching && widget.onSettingsPress != null)
+        _buildItemAction(
+          padding: const EdgeInsets.only(right: 26),
+          child: SvgPicture.asset(
+            Assets.icons.svg.icCloseSeach.path,
+            fit: BoxFit.contain,
+          ),
+          onTap: widget.onSettingsPress,
+        ),
+    ];
+  }
+
+  Widget _buildItemAction({
+    required EdgeInsetsGeometry padding,
+    Widget? child,
+    Function()? onTap,
+  }) {
+    return Padding(
+      padding: padding,
+      child: InkWell(
+        customBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onTap: onTap,
+        child: child,
+      ),
+    );
+  }
+
+  Widget? _buildLeading() {
+    return widget.onBackPress != null
+        ? Container(
+            padding: const EdgeInsets.only(left: 16, right: 0),
+            child: InkWell(
+              onTap: () => widget.onBackPress!(),
+              child: SvgPicture.asset(
+                Assets.icons.svg.icBack.path,
+                fit: BoxFit.contain,
+              ),
+            ),
+          )
+        : null;
+  }
+}
---
+++ lib/presentation/widgets/search_text_field.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/core/extensions/theme.dart';
+
+class SearchField extends StatefulWidget {
+  final String? placeholder;
+  final double fontSize;
+  final double backgroundOpacity;
+  final double placeholderOpacity;
+  final EdgeInsetsGeometry padding;
+  final Color? backgroundColor;
+  final Color cursorColor;
+  final Color? placeholderColor;
+  final BorderRadius borderRadius;
+  final bool isPrefixIcon;
+  final Function(String value) onSearchValue;
+
+  const SearchField({
+    super.key,
+    required this.onSearchValue,
+    this.placeholder,
+    this.fontSize = 16,
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+    this.padding = const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+    this.backgroundColor,
+    this.placeholderColor,
+    this.backgroundOpacity = 0.2,
+    this.placeholderOpacity = 0.5,
+    this.isPrefixIcon = false,
+    this.cursorColor = Colors.white,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<SearchField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: widget.placeholder,
+        prefixIcon: widget.isPrefixIcon
+            ? Icon(
+                Icons.search_outlined,
+                color: context.colors.background,
+              )
+            : null,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: widget.borderRadius,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: widget.borderRadius,
+        ),
+        hintStyle: context.typographies.body.copyWith(
+          color: (widget.placeholderColor ?? context.colors.background)
+              .withOpacity(widget.placeholderOpacity),
+          fontSize: widget.fontSize,
+        ),
+        contentPadding: widget.padding,
+        fillColor: (widget.backgroundColor ?? context.colors.background)
+            .withOpacity(widget.backgroundOpacity),
+      ),
+      autocorrect: false,
+      cursorColor: widget.cursorColor,
+      style: context.typographies.body.copyWith(fontSize: widget.fontSize),
+      onChanged: widget.onSearchValue,
+    );
+  }
+}
